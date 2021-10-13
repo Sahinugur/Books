28f8a8572097b9202cc0f10682b03ff61107a18b
ugur sahin <ugurlao@gmail.com>
2021-10-13 13:46:32 +0200

    books first init

---
+++ src/context/BookContext.js
@@ -0,0 +1,90 @@
+import React from "react";
+
+export const BookContext = React.createContext();
+
+class BookContextProvider extends React.Component {
+  state = {
+    books: [
+      {
+        title: "Kırmızı Pazartesi",
+        author: "Gabriel Garcia Marquez",
+        pageNum: 296,
+        imageURL:
+          "https://i.idefix.com/cache/600x600-0/originals/0000000064101-1.jpg",
+        topic:
+          "1968-1970 yılları arasında geçen olaylar, o günün toplumsal gerçeklerini de satırlara taşıyor. Ama romanın odağında bu toplumsal olaylar değil üçlü bir aşk var. Gençliğin rüzgârıyla hareketlenen İmkânsızın Şarkısını ölümle erken karşılaşan gençlerin hayatı yönlendiriyor. Hiçbir şeyin önem taşımadığı, amaçsızlığın ağır bastığı, özgür seksin kol gezdiği bir öğrenci hayatı... Ama diğer yanda da yoğun duygular var... İmkânsız aşklar, imkânsız şarkılar söyleten. Hemen hemen her Japon gencinin okuduğu roman anayurdu dışında da çok kişi tarafından sahipleniliyor.",
+      },
+      {
+        title: "Şeker Portakalı",
+        author: "Jose Muro de Vasconselos",
+        pageNum: 200,
+        imageURL:
+          "https://i.idefix.com/cache/600x600-0/originals/0000000064031-1.jpg",
+        topic:
+          "İrlandalı yazar Bram Stoker’ın, iki taraf arasındaki bu irade ve güç çatışmasını işlediği ve korku edebiyatının başyapıtlarından biri sayılan Dracula, yayımlanmasının üzerinden yüz yılı aşkın süre geçmesine karşın, bugün de aynı ilgiyle okunuyor.",
+      },
+      {
+        title: "En Uzun Yüzyıl",
+        author: "İlber Ortaylı",
+        pageNum: 296,
+        imageURL:
+          "https://i.idefix.com/cache/600x600-0/originals/0001744876001-1.jpg",
+        topic:
+          "1968-1970 yılları arasında geçen olaylar, o günün toplumsal gerçeklerini de satırlara taşıyor. Ama romanın odağında bu toplumsal olaylar değil üçlü bir aşk var. Gençliğin rüzgârıyla hareketlenen İmkânsızın Şarkısını ölümle erken karşılaşan gençlerin hayatı yönlendiriyor. Hiçbir şeyin önem taşımadığı, amaçsızlığın ağır bastığı, özgür seksin kol gezdiği bir öğrenci hayatı... Ama diğer yanda da yoğun duygular var... İmkânsız aşklar, imkânsız şarkılar söyleten. Hemen hemen her Japon gencinin okuduğu roman anayurdu dışında da çok kişi tarafından sahipleniliyor.",
+      },
+      {
+        title: "Dracula",
+        author: "Bram Stoker",
+        pageNum: 200,
+        imageURL:
+          "https://i.idefix.com/cache/600x600-0/originals/0001828853001-1.jpg",
+        topic:
+          "İrlandalı yazar Bram Stoker’ın, iki taraf arasındaki bu irade ve güç çatışmasını işlediği ve korku edebiyatının başyapıtlarından biri sayılan Dracula, yayımlanmasının üzerinden yüz yılı aşkın süre geçmesine karşın, bugün de aynı ilgiyle okunuyor.",
+      },
+      {
+        title: "Karamazov Kardeşler",
+        author: "Fyodor Mihayloviç Dostoyevski",
+        pageNum: 200,
+        imageURL:
+          "https://i.idefix.com/cache/600x600-0/originals/0001803800001-1.jpg",
+        topic:
+          "İrlandalı yazar Bram Stoker’ın, iki taraf arasındaki bu irade ve güç çatışmasını işlediği ve korku edebiyatının başyapıtlarından biri sayılan Dracula, yayımlanmasının üzerinden yüz yılı aşkın süre geçmesine karşın, bugün de aynı ilgiyle okunuyor.",
+      },
+      {
+        title: "Sultanın Korsanları",
+        author: "Emrah Safa Gürkan",
+        pageNum: 296,
+        imageURL:
+          "https://i.idefix.com/cache/600x600-0/originals/0001780787001-1.jpg",
+        topic:
+          "1968-1970 yılları arasında geçen olaylar, o günün toplumsal gerçeklerini de satırlara taşıyor. Ama romanın odağında bu toplumsal olaylar değil üçlü bir aşk var. Gençliğin rüzgârıyla hareketlenen İmkânsızın Şarkısını ölümle erken karşılaşan gençlerin hayatı yönlendiriyor. Hiçbir şeyin önem taşımadığı, amaçsızlığın ağır bastığı, özgür seksin kol gezdiği bir öğrenci hayatı... Ama diğer yanda da yoğun duygular var... İmkânsız aşklar, imkânsız şarkılar söyleten. Hemen hemen her Japon gencinin okuduğu roman anayurdu dışında da çok kişi tarafından sahipleniliyor.",
+      },
+    ],
+  };
+
+  render() {
+    return (
+      <BookContext.Provider value={this.state}>
+        {this.props.children}
+      </BookContext.Provider>
+    );
+  }
+}
+
+export default BookContextProvider;
+
+// 1-) Default Value ---->this.context
+
+//- Context nesnesi olusturduk
+//- Baslangic degeri verdik ve export ettik
+//- Kullanmak istedigimiz component`a import ettik
+//-Context type yardimiyla contexti ilgili component`a atadaik(static contextType = BookContext)
+//-this.context yardimiyla ilgili veriye ulastik
+
+// 2-) Provider ----------->Consumer
+
+//-Class component olusturduk ve veriyi state icine yerlestirdik
+//-Contexte ait Provider ile state`te bulunan veriyi aldik
+//-Parent-Child iliskisi {this.props.children}
+//-Veriyi gönderecegimiz component`à Context import ediyoruz
+//-Render icerisinde Consumer ile Provider dan gelene veriyi yakaliyoruz
